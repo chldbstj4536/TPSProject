c40c46a586ea8007d8bfad4908ee8f70f6c9f807
최윤서 <chldbstj4536@gmail.com>
2023-07-20 22:38:44 +0900

    BP_TPSPlayer.cpp에서  컴포넌트 설정

--- Source/TPSProject/Private/TPSPlayer.cpp
+++ Source/TPSProject/Private/TPSPlayer.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TPSPlayer.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
 
 // Sets default values
 ATPSPlayer::ATPSPlayer()
@@ -9,6 +10,20 @@ ATPSPlayer::ATPSPlayer()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->SetRelativeLocation(FVector(0, 70, 90));
+	SpringArmComponent->TargetArmLength = 400;
+
+	TPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	TPSCameraComponent->SetupAttachment(SpringArmComponent);
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SkeletalMeshFinder(TEXT("SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'"));
+	if (SkeletalMeshFinder.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(SkeletalMeshFinder.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0.0, 0.0, -90.0), FRotator(0.0, -90.0, 0.0));
+	}
 }
 
 // Called when the game starts or when spawned
